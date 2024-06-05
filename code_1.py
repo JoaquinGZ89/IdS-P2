9303244d69bb608593630493afe31c66d2e5b733
Joaquin Gonzalez Zago <jgzago89@gmail.com>
2024-06-05 14:16:14 -0300

    creacion main.py para fastapi y modificacion a code_1.py

--- code_1.py
+++ code_1.py
@@ -1,6 +1,7 @@
 def sum(a,b):
     return a + b
 
-suma = sum(4,6)
-print(f'El resultado es: {suma}')
+if __name__ == "__main__":
+    suma = sum(4, 6)
+    print(f'El resultado es: {suma}')
 
---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI # type: ignore
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+from code_1 import sum
+
+@app.get("/sum")
+def get_sum(a: int, b: int):
+    return {"sum": sum(a, b)}
