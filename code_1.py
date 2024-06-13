8e7fe9c59303306cdbc62da3e46e90db92559948
Axel <barabasaxel@gmail.com>
2024-06-13 18:34:37 -0300

    error inducido

--- code_1.py
+++ code_1.py
@@ -1,9 +1,8 @@
 def sum(a, b):
-    return a + b
+    return a - b
 
 if __name__ == "__main__":
     suma = sum(4, 6)
     print(f'El resultado es: {suma}')
 
-322443544354354
 #Prueba funcionamiento 4
